1c9e6dcad2ec43d46f30a47d06f8ba919c307729
Cherry <dndwls9252@naver.com>
2021-07-03 21:15:47 +0900

    chore: eslint 및 prettier 설정 (#4)

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
