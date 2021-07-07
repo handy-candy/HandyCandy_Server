a5f6e0cb41e7f1fe2be4d26b1fbb0436937db5f5
YEJIN-LILY <gristar98@naver.com>
2021-07-07 14:30:37 +0900

    feat: 유저 토큰 받기 중간커밋

--- src/api/userInfo.ts
+++ src/api/userInfo.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
-import { check, validationResult } from 'express-validator';
 import Candy from '../models/Candy';
 import User from '../models/User';
+import auth from '../middleware/auth';
 
 const router = Router();
 
@@ -10,26 +10,26 @@ const router = Router();
  * @desc Get user info by user ID
  */
 
-router.get('/', async (req: Request, res: Response) => {
+router.get('/', auth, async (req: Request, res: Response) => {
   try {
-    const candies = await Candy.find({ user_id: req.headers.user_id });
-    const user_nickname = await User.findById(req.headers.user_id).select({ nickname: 1, _id: 0 });
+    const candies = await Candy.find({ user_id: req.body.user.id });
+    const user_nickname = await User.findById(req.body.user.id).select({ nickname: 1, _id: 0 });
     const candyCount = candies.length;
     const date = new Date();
     const month = date.getMonth() + 1;
 
-    var banner, candy_count_phrase, phrase;
+    let banner, candy_count_phrase, phrase;
 
-    if (candyCount == 0) {
-      phrase = user_nickname['nickname'] + '님만의 캔디처럼 달콤한 보상을 담아보세요';
-      candy_count_phrase = '';
-      banner = '비어있는 병을 채워주세요!';
+    if (!candyCount) {
+      phrase = `${user_nickname['nickname']}님만의 캔디처럼 달콤한 보상을 담아보세요`;
+      candy_count_phrase = ``;
+      banner = `비어있는 병을 채워주세요!`;
     } else if (candyCount % 10 == 0) {
-      candy_count_phrase = '벌써 ' + candyCount + '개!';
-      banner = '풍부했던 날을 보내셨네요';
+      candy_count_phrase = `벌써 ${candyCount}개!`;
+      banner = `풍부했던 날을 보내셨네요`;
     } else {
-      candy_count_phrase = candyCount + '개의 캔디!';
-      banner = '행복을 더 담아보세요';
+      candy_count_phrase = `${candyCount}개의 캔디!`;
+      banner = `행복을 더 담아보세요`;
     }
 
     res.json({
