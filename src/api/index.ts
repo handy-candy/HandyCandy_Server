23fcafea811d29787dfaeab760c1dfc4e6ed2376
Jeong-Hyowon <dndwls9252@naver.com>
2021-07-15 03:06:21 +0900

    refactor: api index 적용 및 controller로 로직 분리

---
+++ src/api/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.use('/candies', require('./candies'));
+router.use('/userInfo', require('./userInfo'));
+router.use('/users', require('./user'));
+router.use('/category', require('./category'));
+router.use('/candy', require('./candy'));
+
+export default router;
