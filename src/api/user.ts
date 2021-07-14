8aa56724c17926a19d2df261fdf301709e59624c
Cherry <dndwls9252@naver.com>
2021-07-15 03:09:12 +0900

    refactor: 회원가입, 로그인 DTO 적용 후 api -> contoller -> service 분리 (#101)

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,79 +1,24 @@
-import express, { Request, Response } from 'express';
-import { check, validationResult } from 'express-validator';
-import { SignInDto, SignUpDto } from '../dto/user.dto';
-import { UserService } from '../services';
+import express from 'express';
+import { check } from 'express-validator';
+import { signIn, signUp } from '../controllers';
 const router = express.Router();
 
-/**
- *  @route Post api/users/signUp
- *  @desc Register User
- *  @access Public
- */
-router.post(
-  '/signUp',
-  [
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
-    check('nickname', 'Name is required').not().isEmpty(),
-    check('gender', 'Gender is required').not().isEmpty(),
-    check('birth', 'Birth is required').not().isEmpty(),
-    check('notice_agreement', 'Notice agreement is required').not().isEmpty(),
-  ],
-  async (req: Request, res: Response) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+const check_signup = [
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+  check('nickname', 'Name is required').not().isEmpty(),
+  check('gender', 'Gender is required').not().isEmpty(),
+  check('birth', 'Birth is required').not().isEmpty(),
+  check('notice_agreement', 'Notice agreement is required').not().isEmpty(),
+];
 
-    const signup_dto: SignUpDto = {
-      email: req.body.email,
-      password: req.body.password,
-      nickname: req.body.nickname,
-      gender: req.body.gender,
-      birth: req.body.birth,
-      notice_agreement: req.body.notice_agreement,
-    };
+const check_signin = [
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password is required').exists(),
+];
 
-    const result = await UserService.signUp(signup_dto);
-    if (result.message === 'User already exists') {
-      return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
-    } else if (result.message === 'Server Error') {
-      res.status(500).send('Server Error');
-    } else {
-      res.status(200).json({ token: result });
-    }
-  },
-);
+router.post('/signUp', check_signup, signUp);
 
-/**
- *  @route Post api/users/signIn
- *  @desc Login User
- *  @access Public
- */
-router.post(
-  '/signIn',
-  [check('email', 'Please include a valid email').isEmail(), check('password', 'Password is required').exists()],
-  async (req: Request, res: Response) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const signin_dto: SignInDto = {
-      email: req.body.email,
-      password: req.body.password,
-    };
-
-    const result = await UserService.signIn(signin_dto);
-    if (result.message === 'Invalid Credentials') {
-      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
-    } else if (result.message === 'Server Error') {
-      res.status(500).send('Server Error');
-    } else {
-      res.status(200).json({ token: result });
-    }
-  },
-);
+router.post('/signIn', check_signin, signIn);
 
 module.exports = router;
