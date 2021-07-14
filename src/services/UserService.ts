c4c02dd2e62ccc043f17bf4ef556c493b86fc856
Jeong-Hyowon <dndwls9252@naver.com>
2021-07-15 01:39:58 +0900

    feat: 유저 회원가입, 로그인 DTO 리팩토링

---
+++ src/services/UserService.ts
@@ -0,0 +1,71 @@
+import { SignInDto, SignUpDto } from '../dto/user.dto';
+import User from '../models/User';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import config from '../config';
+export class UserService {
+  static async signUp(user_dto: SignUpDto) {
+    try {
+      let user = await User.findOne({ email: user_dto.email });
+
+      if (user) {
+        return {
+          message: 'User already exists',
+        };
+      }
+
+      user = new User(user_dto);
+
+      await user.save();
+      const salt = await bcrypt.genSalt(10);
+      user.salt = salt;
+      user.password = await bcrypt.hash(user_dto.password, salt);
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = await jwt.sign(payload, config.jwtSecret, { expiresIn: '7d' });
+      return token;
+    } catch (err) {
+      console.error(err.message);
+      return {
+        message: 'Server Error',
+      };
+    }
+  }
+
+  static async signIn(user_dto: SignInDto) {
+    try {
+      console.log('user', user_dto);
+      let user = await User.findOne({ email: user_dto.email });
+
+      if (!user) {
+        return {
+          message: 'Invalid Credentials',
+        };
+      }
+
+      const is_match = await bcrypt.compare(user_dto.password, user.password);
+      if (!is_match) {
+        return {
+          message: 'Invalid Credentials',
+        };
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = await jwt.sign(payload, config.jwtSecret, { expiresIn: '7d' });
+      return token;
+    } catch (err) {
+      console.error(err.message);
+      return {
+        message: 'Server Error',
+      };
+    }
+  }
+}
