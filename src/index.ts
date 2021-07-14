d17b972a9c67832641ae71b56df0f18b8494e751
bak seol <moonleaf0213@naver.com>
2021-07-14 20:53:01 +0900

    chore: index.ts파일 수정

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const cookieParser = require('cookie-parser');
 const corsOptions = {
-  origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
+  origin: ['http://localhost:3000', "https://handycandy-client-handycandy-web.vercel.app/" ], // 허락하고자 하는 요청 주소
   credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
 };
 
