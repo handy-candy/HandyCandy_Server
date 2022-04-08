f6cff8e2106869f1a2a8ab74d4214ef48b634a0d
silver <silver36c@gmail.com>
2022-04-09 04:16:09 +0900

    feat: 스웨거 모델 추가 #121
    User, Category, Candy

--- src/models/Candy.ts
+++ src/models/Candy.ts
@@ -1,3 +1,40 @@
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Candy:
+ *        type: object
+ *        required:
+ *          - name
+ *          - user_id
+ *          - category_id
+ *        properties:
+ *          name:
+ *            type: String
+ *          shopping_link:
+ *            type: String
+ *          shopping_link_image:
+ *            type: String
+ *          shopping_link_name:
+ *            type: String
+ *          price:
+ *            type: String
+ *          candy_image_url:
+ *            type: String
+ *          notice_agreement:
+ *            type: Boolean
+ *          reward_planned_at:
+ *            type: Date
+ *          reward_completed_at:
+ *            type: Date
+ *          user_id:
+ *            type: mongoose.Schema.Types.ObjectId
+ *            ref: 'User'
+ *          category_id:
+ *            type: mongoose.Schema.Types.ObjectId
+ *            ref: 'User'
+ */
+
 import mongoose from 'mongoose';
 import { ICandy } from '../interfaces/ICandy';
 
