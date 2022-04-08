eafb788c8b67527f269c7f02f5f15144640045ba
silver <silver36c@gmail.com>
2022-04-09 04:16:09 +0900

    feat: 스웨거 모델 추가
    User, Category, Candy

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,36 @@
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      User:
+ *        type: object
+ *        required:
+ *          - email
+ *          - nickname
+ *        properties:
+ *          email:
+ *            type: String
+ *            format: email
+ *            description: unique
+ *          password:
+ *            type: String
+ *          salt:
+ *            type: String
+ *          nickname:
+ *            type: String
+ *          gender:
+ *            type: Number
+ *          birth:
+ *            type: Number
+ *          notice_agreement:
+ *            type: Boolean
+ *          sns_id:
+ *            type: String
+ *          provider:
+ *            type: String
+ */
+
+
 import mongoose from 'mongoose';
 import { IUser } from '../interfaces/IUser';
 
