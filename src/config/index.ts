bed8a08c0976b96199f74286409d9a17135aff0b
silver <silver36c@gmail.com>
2022-05-08 09:32:56 +0900

    feat: production 모드 설정 #121
    #121

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 
-// Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+// // Set the NODE_ENV to 'development' by default
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
 if (envFound.error) {
